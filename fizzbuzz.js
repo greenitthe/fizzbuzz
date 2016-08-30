6b90c73584235d0018ef8cdab7f441320d9a6eef
greenitthe <greenitthe@gmail.com>
2016-08-29 19:11:55 -0500

    Added extra credit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,36 @@
 /**/
+//Extra credit: function that goes up to it's argument 'max' instead of just 100
+function fizzbuzz (max) {
+  for (var i = 1; i <= max; i++) {
+    //Temporary variable to hold the message we will log
+    var message = "";
+    //test for multiples of 3 using the modulo operator (%)
+    if (i % 3 == 0) {
+      message += "Fizz";
+    }
+    //test for multiples of 5
+    //if the number was a multiple of 3 already will be FizzBuzz, if wasn't a multiple of 3 will be Buzz
+    if (i % 5 == 0) {
+      message += "Buzz";
+    }
+    //test for the message still being empty
+    if (message == "") {
+      //since its empty, throw in our current index
+      message = i;
+    }
+    //log whatever is in the message
+    console.log(message);
+  }
+}
+
+fizzbuzz(5);
+fizzbuzz(13);
+fizzbuzz(89);
+fizzbuzz(233);
+
+/**/
+
+/**
 //For loop so we can walk through the 100 numbers
 for (var i = 1; i <= 100; i++) {
   //Temporary variable to hold the message we will log
